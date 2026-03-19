f995a1b4574e982b4e97dfd2473684b677a93e5d
Naman Patel <namanp2166@gmail.com>
2026-03-20 02:22:13 +0530

    feature: added student data

---
+++ NamanPatel_frontend/js/namanpatel_student_analyzer.js
@@ -0,0 +1,38 @@
+// I created an array of students with their data as already given in assignment
+
+const students = [
+    {
+      name: "Lalit",
+      marks: [
+        { subject: "Math", score: 78 },
+        { subject: "English", score: 82 },
+        { subject: "Science", score: 74 },
+        { subject: "History", score: 69 },
+        { subject: "Computer", score: 88 }
+      ],
+      attendance: 82
+    },
+    {
+      name: "Rahul",
+      marks: [
+        { subject: "Math", score: 90 },
+        { subject: "English", score: 85 },
+        { subject: "Science", score: 80 },
+        { subject: "History", score: 76 },
+        { subject: "Computer", score: 92 }
+      ],
+      attendance: 91
+    },
+    {
+      name: "Naman",
+      marks: [
+        { subject: "Math", score: 92 },
+        { subject: "English", score: 86 },
+        { subject: "Science", score: 75 },
+        { subject: "History", score: 72 },
+        { subject: "Computer", score: 95 }
+      ],
+      attendance: 86
+    },
+    
+  ];
