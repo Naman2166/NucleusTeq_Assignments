b631a357e9c48d8f01d65d0d69ee6ff1aa3d10cd
Naman Patel <namanp2166@gmail.com>
2026-03-22 20:16:50 +0530

    feature: Added dom elements for dashboard

--- NamanPatel_frontend/mini_app/script.js
+++ NamanPatel_frontend/mini_app/script.js
@@ -8,3 +8,33 @@ let products = [
   { id: 6, name: "jeans", price: 1000, stock: 20, category: "Clothing" },
   { id: 7, name: "PS5", price: 100, stock: 20, category: "Electronics" },
 ];
+
+
+
+//here we are getting all required dom elements
+
+//getting Product grid elements
+const productGrid = document.getElementById("productGrid");
+const noProductMessage = document.getElementById("noProductMessage");
+
+//add product form elemnst
+const addProductForm = document.getElementById("addProductForm");
+const productName = document.getElementById("productName");
+const productPrice = document.getElementById("productPrice");
+const productStock = document.getElementById("productStock");
+const productCategory = document.getElementById("productCategory");
+
+//getting analytics elememts
+const totalProducts = document.getElementById("totalProducts");
+const totalValue = document.getElementById("totalValue");
+const outOfStock = document.getElementById("outOfStock");
+const categoryCount = document.getElementById("categoryCount");
+
+//getting controls elements
+const searchInput = document.getElementById("search");
+const categoryFilter = document.getElementById("categoryFilter");
+const lowStockFilter = document.getElementById("lowStockFilter");
+const sortSelect = document.getElementById("sort");
+const clearBtn = document.getElementById("clearAllBtn");
+
+
