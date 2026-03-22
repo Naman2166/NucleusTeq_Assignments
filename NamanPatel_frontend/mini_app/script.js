8712e03f4fdee14cb1b9fa1adfbb41ee450ff9da
Naman Patel <namanp2166@gmail.com>
2026-03-22 19:57:04 +0530

    feature: Added deafult product data

---
+++ NamanPatel_frontend/mini_app/script.js
@@ -0,0 +1,10 @@
+// it is a Default Product data which will be visible even when no product is there
+let products = [
+  { id: 1, name: "Laptop", price: 50000, stock: 8, category: "Electronics" },
+  { id: 2, name: "Headphones", price: 1500, stock: 3, category: "Electronics" },
+  { id: 3, name: "Shirt", price: 800, stock: 10, category: "Clothing" },
+  { id: 4, name: "Shoes", price: 2500, stock: 0, category: "Footwear" },
+  { id: 5, name: "Notebook", price: 100, stock: 20, category: "Stationery" },
+  { id: 6, name: "jeans", price: 1000, stock: 20, category: "Clothing" },
+  { id: 7, name: "PS5", price: 100, stock: 20, category: "Electronics" },
+];
