e45773a91c7c693830cd4ef18fe4da0cdb6c0619
Naman Patel <namanp2166@gmail.com>
2026-04-15 05:59:14 +0530

    Added initial springboot project setup

---
+++ NamanPatel_java_training/session3/spring-and-rest-assignment/src/main/java/com/naman/assignment/SpringAndRestAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.naman.assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAndRestAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAndRestAssignmentApplication.class, args);
+	}
+
+}
