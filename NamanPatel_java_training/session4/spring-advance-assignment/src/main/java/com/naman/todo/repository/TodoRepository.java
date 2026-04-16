5834425de99d624e1b97a1d4f8403706c0598bd6
Naman Patel <namanp2166@gmail.com>
2026-04-17 03:42:05 +0530

    Add TodoRepository using JpaRepository

--- NamanPatel_java_training/session4/spring-advance-assignment/src/main/java/com/naman/todo/repository/TodoRepository.java
+++ NamanPatel_java_training/session4/spring-advance-assignment/src/main/java/com/naman/todo/repository/TodoRepository.java
@@ -1,4 +1,11 @@
 package com.naman.todo.repository;
 
-public class TodoRepository {
+import com.naman.todo.entity.Todo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+// Repository for Todo entity to perform database operations
+// it extends JpaRepository to use build-in CRUD methods
+@Repository
+public interface TodoRepository extends JpaRepository<Todo, Long> {
 }
