579311b21b96ffb8651cc8dc977cb6f8bf8ff4ad
Naman Patel <namanp2166@gmail.com>
2026-04-19 06:29:22 +0530

    Add logging in TodoService Class

--- NamanPatel_java_training/session4/spring-advance-assignment/src/main/java/com/naman/todo/service/TodoService.java
+++ NamanPatel_java_training/session4/spring-advance-assignment/src/main/java/com/naman/todo/service/TodoService.java
@@ -7,6 +7,8 @@ import com.naman.todo.enums.TodoStatus;
 import com.naman.todo.exception.InvalidStatusException;
 import com.naman.todo.exception.TodoNotFoundException;
 import com.naman.todo.repository.TodoRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -25,10 +27,16 @@ public class TodoService {
         this.todoRepository = todoRepository;
     }
 
+    //Logger for TodoService class
+    private static final Logger logger = LoggerFactory.getLogger(TodoService.class);
+
 
 
     // method to create new todo
     public TodoResponseDTO createTodo(TodoRequestDTO dto) {
+
+        //log message before creating todo
+        logger.info("Creating todo: {}", dto.getTitle());
 
         Todo todo = new Todo();
 
@@ -38,7 +46,7 @@ public class TodoService {
         todo.setCreatedAt(LocalDateTime.now());        //creation time set automatically
 
         // applying validation
-        if (dto.getStatus() == null) {              //default status will be pending
+        if (dto.getStatus() == null) {                //default status will be pending
             todo.setStatus(TodoStatus.PENDING);
         }
         else {
@@ -47,6 +55,9 @@ public class TodoService {
 
         Todo saved = todoRepository.save(todo);      //save to repo
 
+        //log message after saving todo
+        logger.info("Todo created with id: {}", saved.getId());
+
         return mapToResponseDTO(saved);                  //return dto after mapping saved entity to dto
     }
 
@@ -54,16 +65,22 @@ public class TodoService {
 
     // method to get all todos
     public List<TodoResponseDTO> getAllTodos() {
+
+        logger.info("Fetching all todos");              //log message for fetching all todos
+
         return todoRepository.findAll()
                 .stream()
-                .map(todo -> mapToResponseDTO(todo))        //mapping each todo into Dto
-                .collect(Collectors.toList());            //return list
+                .map(todo -> mapToResponseDTO(todo))    //mapping each todo into Dto
+                .collect(Collectors.toList());                //return list
     }
 
 
 
     // method to get todo by id
     public TodoResponseDTO getTodoById(Long id) {
+
+        logger.info("Fetching todo with id: {}", id);         // log entry for fetching todo by id
+
         Todo todo = todoRepository.findById(id)
                 .orElseThrow(() -> new TodoNotFoundException("Todo not found with id: " + id ));      //throw exception if todo not found
 
@@ -76,6 +93,9 @@ public class TodoService {
     public TodoResponseDTO updateTodo(Long id, TodoRequestDTO dto) {
         Todo todo = todoRepository.findById(id)
                 .orElseThrow(() -> new TodoNotFoundException("Todo not found with id: " + id ));
+
+        //logging message before updating todo
+        logger.info("Updating todo with id: {}", id);
 
         //mapping dto to entity
         todo.setTitle(dto.getTitle());
@@ -103,6 +123,9 @@ public class TodoService {
 
         Todo updated = todoRepository.save(todo);
 
+        //log message after updating todo
+        logger.info("Todo updated with id: {}", updated.getId());
+
         return mapToResponseDTO(updated);
     }
 
@@ -113,7 +136,13 @@ public class TodoService {
         Todo todo = todoRepository.findById(id)
                 .orElseThrow(() -> new TodoNotFoundException("Todo not found with id: " + id ));
 
+        //log message before deletion
+        logger.info("Deleting todo with id: {}", id);
+
         todoRepository.delete(todo);
+
+        //log message after deletion
+        logger.info("todo deleted with id: {}", id);
     }
 
 
