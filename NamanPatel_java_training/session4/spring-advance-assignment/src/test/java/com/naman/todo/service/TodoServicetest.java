69b248345117097b6dc50b95ec68da0f488df4ad
Naman Patel <namanp2166@gmail.com>
2026-04-19 06:55:28 +0530

    Add unit test for getTodoById and getAllTodos

--- NamanPatel_java_training/session4/spring-advance-assignment/src/test/java/com/naman/todo/service/TodoServicetest.java
+++ NamanPatel_java_training/session4/spring-advance-assignment/src/test/java/com/naman/todo/service/TodoServicetest.java
@@ -10,9 +10,12 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.util.List;
+import java.util.Optional;
+
+
 import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.*;
-
 
 //unit test class for todoService
 class TodoServiceTest {
@@ -61,6 +64,68 @@ class TodoServiceTest {
         // Verify repository save method and notification sent methods are called once only
         verify(todoRepository, times(1)).save(any(Todo.class));
         verify(notificationServiceClient, times(1)).sendNotification(anyString());
+    }
+
+
+
+    // Test for getTodoById method
+    @Test
+    void testGetTodoById() {
+
+        // create fake input id
+        Long id = 1L;
+
+        // creates fake output object
+        Todo todo = new Todo();
+        todo.setId(1L);
+        todo.setTitle("Test Title");
+        todo.setDescription("Test Description");
+        todo.setStatus(TodoStatus.PENDING);
+
+        // set behaviour for repository findById operation
+        when(todoRepository.findById(id)).thenReturn(Optional.of(todo));
+
+        // call service class method
+        TodoResponseDTO result = todoService.getTodoById(id);
+
+        // checking and verifying the results
+        assertNotNull(result);
+        assertEquals("Test Title", result.getTitle());
+        verify(todoRepository, times(1)).findById(id);
+    }
+
+
+
+    // Test for getAllTodos method
+    @Test
+    void testGetAllTodos() {
+
+        // create fake output todo list
+        Todo todo1 = new Todo();
+        todo1.setId(1L);
+        todo1.setTitle("Task 1");
+        todo1.setDescription("Description 1");
+        todo1.setStatus(TodoStatus.PENDING);
+
+        Todo todo2 = new Todo();
+        todo2.setId(2L);
+        todo2.setTitle("Task 2");
+        todo2.setDescription("Description 2");
+        todo2.setStatus(TodoStatus.COMPLETED);
+
+        List<Todo> todoList = List.of(todo1, todo2);
+
+        // set behaviour
+        when(todoRepository.findAll()).thenReturn(todoList);
+
+        // call getAllTodos method
+        List<TodoResponseDTO> result = todoService.getAllTodos();
+
+        // verify result
+        assertNotNull(result);
+        assertEquals(2, result.size());
+        assertEquals("Task 1", result.get(0).getTitle());
+        verify(todoRepository, times(1)).findAll();
     }
 
 }
