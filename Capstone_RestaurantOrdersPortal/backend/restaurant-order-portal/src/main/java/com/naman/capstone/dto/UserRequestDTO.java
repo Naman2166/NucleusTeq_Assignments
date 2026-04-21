e78e78498907e2ab981d23156b4da6ddfb536973
Naman Patel <namanp2166@gmail.com>
2026-04-21 20:16:02 +0530

    add User repository and and user dto with validation

---
+++ Capstone_RestaurantOrdersPortal/backend/restaurant-order-portal/src/main/java/com/naman/capstone/dto/UserRequestDTO.java
@@ -0,0 +1,41 @@
+package com.naman.capstone.dto;
+
+import com.naman.capstone.enums.Role;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+//Represents the data that comes from the client for registration
+@Getter
+@NoArgsConstructor
+@Setter
+@AllArgsConstructor
+public class UserRequestDTO {
+
+    @NotBlank(message = "First name cannot be empty")
+    private String firstName;
+
+    @NotBlank(message = "Last name cannot be empty")
+    private String lastName;
+
+    @NotBlank(message = "Email cannot be empty")
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @NotBlank(message = "Password cannot be empty")
+    private String password;
+
+    @NotBlank(message = "Phone number cannot be empty")
+    @Pattern(regexp = "^[0-9]{10}$", message = "Phone number must be 10 digits")
+    private String phoneNumber;
+
+    @NotNull(message = "Role is required")
+    private Role role;
+
+}
--- Capstone_RestaurantOrdersPortal/backend/restaurant-order-portal/src/main/java/com/naman/capstone/repository/UserRepository.java
+++ Capstone_RestaurantOrdersPortal/backend/restaurant-order-portal/src/main/java/com/naman/capstone/repository/UserRepository.java
@@ -1,4 +1,15 @@
 package com.naman.capstone.repository;
 
-public class UserRepository {
+import com.naman.capstone.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+//Repository for User entity to perform database operations
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    //finds user by email
+    Optional<User> findByEmail(String email);
+
+    //checks if email already exists
+    boolean existsByEmail(String email);
 }
