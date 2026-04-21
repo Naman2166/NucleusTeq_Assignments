b06c81a5d6475e7a3543edb9d6e96b64038c2c4f
Naman Patel <namanp2166@gmail.com>
2026-04-21 20:20:42 +0530

    add basic security configuration

---
+++ Capstone_RestaurantOrdersPortal/backend/restaurant-order-portal/src/main/java/com/naman/capstone/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.naman.capstone.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+//Security configuration for controlling API access
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.POST, "/api/users/register").permitAll()
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
+}
