f9d66e83f3892732512148f23fd9bc71cf760cad
Naman Patel <namanp2166@gmail.com>
2026-04-23 06:12:56 +0530

    add java doc in main file

--- Capstone_RestaurantOrdersPortal/backend/restaurant-order-portal/src/main/java/com/naman/capstone/RestaurantOrderPortalApplication.java
+++ Capstone_RestaurantOrdersPortal/backend/restaurant-order-portal/src/main/java/com/naman/capstone/RestaurantOrderPortalApplication.java
@@ -3,6 +3,9 @@ package com.naman.capstone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main entry point to start the application.
+ */
 @SpringBootApplication
 public class RestaurantOrderPortalApplication {
 
