0353e2662cbe0ddb8420c5033aad910d3c3d0196
Naman Patel <namanp2166@gmail.com>
2026-04-23 06:06:25 +0530

    update user controller with register and login API and add App constant file

--- Capstone_RestaurantOrdersPortal/backend/restaurant-order-portal/src/main/java/com/naman/capstone/controller/UserController.java
+++ Capstone_RestaurantOrdersPortal/backend/restaurant-order-portal/src/main/java/com/naman/capstone/controller/UserController.java
@@ -1,32 +1,50 @@
 package com.naman.capstone.controller;
 
-import com.naman.capstone.dto.UserRequestDTO;
-import com.naman.capstone.dto.UserResponseDTO;
+import com.naman.capstone.constant.AppConstants;
+import com.naman.capstone.dto.request.LoginRequestDTO;
+import com.naman.capstone.dto.request.RegisterRequestDTO;
+import com.naman.capstone.dto.response.LoginResponseDTO;
+import com.naman.capstone.dto.response.UserResponseDTO;
 import com.naman.capstone.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-
-//this class handles all API request related to user
+/**
+ * This class handles all API requests related to users
+ */
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping(AppConstants.USER_BASE_URL)
 public class UserController {
 
     private final UserService userService;
 
     //constructor injection
-    public UserController(UserService userService) {
-        this.userService = userService;
+    public UserController(UserService authService) {
+        this.userService = authService;
     }
 
-    // API to register a new user
-    @PostMapping("/register")
-    public ResponseEntity<UserResponseDTO>  registerUser(@Valid @RequestBody UserRequestDTO dto) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(userService.registerUser(dto));
+    /**
+     * API to register a new user
+     * @param registerRequestDTO registration data from client
+     * @return registered user details
+     */
+    @PostMapping(AppConstants.REGISTER)
+    public ResponseEntity<UserResponseDTO> registerUser(@Valid @RequestBody RegisterRequestDTO registerRequestDTO) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(userService.register(registerRequestDTO));
     }
+
+
+    /**
+     * API for login
+     * @param loginRequestDTO login credentials from client
+     * @return login response containing token and user details
+     */
+    @PostMapping(AppConstants.LOGIN)
+    public ResponseEntity<LoginResponseDTO> loginUser(@Valid @RequestBody LoginRequestDTO loginRequestDTO) {
+        return ResponseEntity.status(HttpStatus.OK).body(userService.login(loginRequestDTO));
+    }
+
 }
+
