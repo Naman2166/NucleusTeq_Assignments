a547a1d58b722a4121983f42134ea1b04746e3a3
Naman Patel <namanp2166@gmail.com>
2026-04-26 17:02:24 +0530

    Add Appconstant class conataining app wide URL constants

--- Capstone_RestaurantOrdersPortal/backend/restaurant-order-portal/src/main/java/com/naman/capstone/constant/AppConstants.java
+++ Capstone_RestaurantOrdersPortal/backend/restaurant-order-portal/src/main/java/com/naman/capstone/constant/AppConstants.java
@@ -9,14 +9,26 @@ public class AppConstants {
 
     private AppConstants() {}
 
-    //User API
+    //Base URL constants
     public static final String USER_BASE_URL = "/api/users";
+    public static final String RESTAURANT_BASE_URL = "/api/restaurants";
+    public static final String CATEGORY_BASE_URL = "/api/categories";
+    public static final String MENU_ITEM_BASE_URL = "/api/menu-items";
+    public static final String CART_BASE_URL = "/api/cart";
+    public static final String ORDER_BASE_URL = "/api/orders";
+
+    //other URL constants
+    public static final String MY_RESTAURANTS = "/my-restaurants";
+
+    //Auth constant
     public static final String REGISTER = "/register";
     public static final String LOGIN = "/login";
 
-    //Restaurant API
-    public static final String RESTAURANT_BASE_URL = "/api/restaurants";
+    //Roles constant
+    public static final String ROLE_OWNER = "RESTAURANT_OWNER";
+    public static final String ROLE_USER = "USER";
 
     //Default Values
     public static final BigDecimal DEFAULT_WALLET_BALANCE = new BigDecimal("1000");
+
 }
