2de21c3caaf98b2f372b666ef54a098bc0f6e66f
Naman Patel <namanp2166@gmail.com>
2026-04-30 02:57:22 +0530

    Add java docs in enums

--- Capstone_RestaurantOrdersPortal/backend/restaurant-order-portal/src/main/java/com/naman/capstone/enums/RestaurantStatus.java
+++ Capstone_RestaurantOrdersPortal/backend/restaurant-order-portal/src/main/java/com/naman/capstone/enums/RestaurantStatus.java
@@ -4,6 +4,14 @@ package com.naman.capstone.enums;
  * this enum represents status of restaurant
  */
 public enum RestaurantStatus {
+
+    /**
+     * restaurant is open and accepting orders
+     */
     OPEN,
+
+    /**
+     * restaurant is closed and not accepting orders
+     */
     CLOSED
 }
--- Capstone_RestaurantOrdersPortal/backend/restaurant-order-portal/src/main/java/com/naman/capstone/enums/Role.java
+++ Capstone_RestaurantOrdersPortal/backend/restaurant-order-portal/src/main/java/com/naman/capstone/enums/Role.java
@@ -4,6 +4,14 @@ package com.naman.capstone.enums;
  * this enum represents the roles available in this application
  */
 public enum Role {
+
+    /**
+     * normal user who can place orders
+     */
     USER,
+
+    /**
+     * restaurant owner who manages restaurants and menu
+     */
     RESTAURANT_OWNER
 }
