ba58de37c25a34e075dc7f4d94a5fc8f6c276cf2
Naman Patel <namanp2166@gmail.com>
2026-04-30 03:08:53 +0530

    Add controller classes for all the services

--- Capstone_RestaurantOrdersPortal/backend/restaurant-order-portal/src/main/java/com/naman/capstone/controller/RestaurantController.java
+++ Capstone_RestaurantOrdersPortal/backend/restaurant-order-portal/src/main/java/com/naman/capstone/controller/RestaurantController.java
@@ -4,10 +4,11 @@ import com.naman.capstone.constant.AppConstants;
 import com.naman.capstone.dto.request.RestaurantRequestDTO;
 import com.naman.capstone.dto.response.RestaurantResponseDTO;
 import com.naman.capstone.entity.User;
-import com.naman.capstone.repository.UserRepository;
 import com.naman.capstone.service.CurrentUserService;
 import com.naman.capstone.service.RestaurantService;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -15,6 +16,9 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+import static com.naman.capstone.constant.AppConstants.MY_RESTAURANTS;
+import static com.naman.capstone.constant.AppConstants.RESTAURANT_ID;
 
 
 /**
@@ -32,19 +36,24 @@ public class RestaurantController {
         this.currentUserService = currentUserService;
     }
 
+    private static final Logger log = LoggerFactory.getLogger(RestaurantController.class);
+
 
     /**
      * Creates a new restaurant
      * @param restaurantRequestDTO data for creating a restaurant
-     * @param userDetails current authenticated user details
+     * @param userDetails authenticated user details
      * @return created restaurant details
      */
     @PostMapping
     public ResponseEntity<RestaurantResponseDTO> createRestaurant(@Valid @RequestBody RestaurantRequestDTO restaurantRequestDTO,
                                                                   @AuthenticationPrincipal UserDetails userDetails){
-        //converting UserDetails to User entity
+        log.info("Create restaurant request received");
         User user = currentUserService.getCurrentUser(userDetails);
-        return ResponseEntity.status(HttpStatus.CREATED).body(restaurantService.createRestaurant(restaurantRequestDTO, user));
+        RestaurantResponseDTO response = restaurantService.createRestaurant(restaurantRequestDTO, user);
+        log.info("Restaurant created successfully by userId={}", user.getId());
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
 
@@ -52,28 +61,35 @@ public class RestaurantController {
      * Updates an existing restaurant by id
      * @param id restaurant id
      * @param restaurantRequestDTO updated restaurant data
-     * @param userDetails current authenticated user
+     * @param userDetails authenticated user
      * @return updated restaurant data
      */
-    @PutMapping("/{id}")
-    public ResponseEntity<RestaurantResponseDTO> updateRestaurant(@PathVariable Long id,
+    @PutMapping(RESTAURANT_ID)
+    public ResponseEntity<RestaurantResponseDTO> updateRestaurant(@PathVariable("restaurantID") Long id,
                                                   @Valid @RequestBody RestaurantRequestDTO restaurantRequestDTO,
                                                   @AuthenticationPrincipal UserDetails userDetails){
+        log.info("Update restaurant request received for restaurantId={}", id);
         User user = currentUserService.getCurrentUser(userDetails);
-        return ResponseEntity.status(HttpStatus.OK).body(restaurantService.updateRestaurant(id, restaurantRequestDTO, user));
+        RestaurantResponseDTO response = restaurantService.updateRestaurant(id, restaurantRequestDTO, user);
+        log.info("Restaurant updated successfully for restaurantId={}", id);
+
+        return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
 
     /**
      * deletes a restaurant by id
      * @param id restaurant id
-     * @param userDetails current authenticated user details
+     * @param userDetails authenticated user details
      * @return restaurant deleted message
      */
-    @DeleteMapping("/{id}")
-    public ResponseEntity<String> deleteRestaurant(@PathVariable Long id, @AuthenticationPrincipal UserDetails userDetails){
+    @DeleteMapping(RESTAURANT_ID)
+    public ResponseEntity<String> deleteRestaurant(@PathVariable("restaurantID") Long id, @AuthenticationPrincipal UserDetails userDetails){
+        log.warn("Delete restaurant request received for restaurantId={}", id);
         User user = currentUserService.getCurrentUser(userDetails);
         restaurantService.deleteRestaurant(id, user);
+        log.info("Restaurant deleted successfully for restaurantId={}", id);
+
         return ResponseEntity.status(HttpStatus.OK).body("Restaurant deleted successfully");
     }
 
@@ -84,19 +100,39 @@ public class RestaurantController {
      */
     @GetMapping
     public ResponseEntity<List<RestaurantResponseDTO>> getAllRestaurants(){
-        return ResponseEntity.status(HttpStatus.OK).body(restaurantService.getAllRestaurants());
+        log.info("Fetching all restaurants");
+        List<RestaurantResponseDTO> response = restaurantService.getAllRestaurants();
+        log.info("Fetched {} restaurants", response.size());
+
+        return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
+    /**
+     * get restaurant by id
+     * @return restaurant object
+     */
+    @GetMapping(RESTAURANT_ID)
+    public ResponseEntity<RestaurantResponseDTO> getRestaurantById(@PathVariable Long id) {
+        log.info("Fetching restaurant for restaurantId={}", id);
+        RestaurantResponseDTO response = restaurantService.getRestaurantById(id);
+        log.info("Restaurant fetched successfully for restaurantId={}", id);
+
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
 
     /**
      * get restaurants of the specific user or owner
-     * @param userDetails current authenticated user details
+     * @param userDetails authenticated user details
      * @return list of user's restaurants
      */
-    @GetMapping(AppConstants.MY_RESTAURANTS)
+    @GetMapping(MY_RESTAURANTS)
     public ResponseEntity<List<RestaurantResponseDTO>> getRestaurantsByOwner(@AuthenticationPrincipal UserDetails userDetails){
+        log.info("Fetching restaurants for current user");
         User user = currentUserService.getCurrentUser(userDetails);
-        return ResponseEntity.status(HttpStatus.OK).body(restaurantService.getRestaurantsByOwner(user));
+        List<RestaurantResponseDTO> response = restaurantService.getRestaurantsByOwner(user);
+        log.info("Fetched all restaurants for userId={}", user.getId());
+
+        return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
 }
--- Capstone_RestaurantOrdersPortal/backend/restaurant-order-portal/src/main/java/com/naman/capstone/controller/UserController.java
+++ Capstone_RestaurantOrdersPortal/backend/restaurant-order-portal/src/main/java/com/naman/capstone/controller/UserController.java
@@ -7,6 +7,8 @@ import com.naman.capstone.dto.response.LoginResponseDTO;
 import com.naman.capstone.dto.response.UserResponseDTO;
 import com.naman.capstone.service.UserService;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,10 +22,12 @@ public class UserController {
 
     private final UserService userService;
 
-    //constructor injection
     public UserController(UserService authService) {
         this.userService = authService;
     }
+
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
+
 
     /**
      * API to register a new user
@@ -32,7 +36,11 @@ public class UserController {
      */
     @PostMapping(AppConstants.REGISTER)
     public ResponseEntity<UserResponseDTO> registerUser(@Valid @RequestBody RegisterRequestDTO registerRequestDTO) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(userService.register(registerRequestDTO));
+        log.info("User registration request received");
+        UserResponseDTO response = userService.register(registerRequestDTO);
+        log.info("User registered successfully with email={}", registerRequestDTO.getEmail());
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
 
@@ -43,7 +51,11 @@ public class UserController {
      */
     @PostMapping(AppConstants.LOGIN)
     public ResponseEntity<LoginResponseDTO> loginUser(@Valid @RequestBody LoginRequestDTO loginRequestDTO) {
-        return ResponseEntity.status(HttpStatus.OK).body(userService.login(loginRequestDTO));
+        log.info("Login request received for email={}", loginRequestDTO.getEmail());
+        LoginResponseDTO response = userService.login(loginRequestDTO);
+        log.info("User logged in successfully with email={}", loginRequestDTO.getEmail());
+
+        return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
 }
