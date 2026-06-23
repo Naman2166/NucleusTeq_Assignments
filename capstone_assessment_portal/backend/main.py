cbee4fea6960aa95a797c2e4e1cd1d36c995a364
Naman Patel <namanp2166@gmail.com>
2026-06-24 04:53:41 +0530

    feat: set up project with requirements and database connection

--- capstone_assessment_portal/backend/main.py
+++ capstone_assessment_portal/backend/main.py
@@ -1,1 +1,11 @@
+from fastapi import Depends, FastAPI
+from app.routes.auth_routes import router as auth_router
 
+app = FastAPI()
+
+# routers
+app.include_router(auth_router)
+
+@app.get("/")
+def home():
+    return {"message": "Server started successfully"}
