82082a66a9b106666ec69c84c16120dc3efdb1d7
Naman Patel <namanp2166@gmail.com>
2026-06-24 06:58:24 +0530

    feat: add CORS middleware

--- capstone_assessment_portal/backend/main.py
+++ capstone_assessment_portal/backend/main.py
@@ -1,7 +1,17 @@
 from fastapi import Depends, FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes.auth_routes import router as auth_router
 
 app = FastAPI()
+
+# CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # routers
 app.include_router(auth_router)
