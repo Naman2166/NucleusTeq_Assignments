a8645f91f8f63793589c954b95360adc942a6fa8
Naman Patel <namanp2166@gmail.com>
2026-07-23 08:11:14 +0530

    Dockerize Assessment Portal and configure cloud database

--- Capstone_Assessment_Portal/backend/main.py
+++ Capstone_Assessment_Portal/backend/main.py
@@ -10,10 +10,15 @@ from app.routes.result_routes import router as result_router
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
 # CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
